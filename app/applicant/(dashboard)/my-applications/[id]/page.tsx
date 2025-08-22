234e87114f0626438b997d883087d94b1a35a172
rpatel <rpatel@usgeocoder.com>
2025-08-22 12:55:11 -0500

    update skeleton loading

--- app/applicant/(dashboard)/my-applications/[id]/page.tsx
+++ app/applicant/(dashboard)/my-applications/[id]/page.tsx
@@ -8,7 +8,8 @@ import { getApplicantInfo, getQuestions } from '@/api/applicant';
 import { customToast } from '@/components/common/toastr';
 import { useEffect, useState, use } from 'react';
 import { useBasic } from '@/context/BasicContext';
-import LoadingSpinner from '@/components/common/loading-spinner';
+
+import { Skeleton } from '@/components/ui/skeleton';
 
 /**
  * ApplicationDetails Component
@@ -101,155 +102,216 @@ export default function ApplicationDetails({ params }: { params: Promise<{ id: s
           data-testid="application-header"
         >
           <div>
-            <p
-              className="text-[18px]/[30px] text-[#0D978B] underline"
-              id="job-title"
-              data-testid="job-title"
-            >
-              {applicantInfo?.job?.title}
-            </p>
-            <div className="mt-[4px] flex gap-[8px]">
-              <span
-                className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
-                id="job-type"
-                data-testid="job-type"
-              >
-                <BriefcaseBusiness className="size-[16px]" />
-                Fulltime
-              </span>
-              <span
-                className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
-                id="job-location"
-                data-testid="job-location"
-              >
-                <MapPin className="size-[16px]" />
-                {country && country.length > 0 ? (country as CountryItem[]).find(item => item.id === applicantInfo?.job?.country_id)?.name || 'Unknown' : 'Unknown'}
-              </span>
-              <span
-                className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
-                id="submission-date"
-                data-testid="submission-date"
-              >
-                <Clock className="size-[16px]" />
-                {moment(applicantInfo?.job?.deadline).format('MMMM DD YYYY')}
-              </span>
-            </div>
+            {loading ? (
+              <>
+                <Skeleton className="h-[30px] w-[200px] bg-gray-200" />
+                <div className="mt-[4px] flex gap-[8px]">
+                  <Skeleton className="h-[18px] w-[80px] bg-gray-200" />
+                  <Skeleton className="h-[18px] w-[100px] bg-gray-200" />
+                  <Skeleton className="h-[18px] w-[120px] bg-gray-200" />
+                </div>
+              </>
+            ) : (
+              <>
+                <p
+                  className="text-[18px]/[30px] text-[#0D978B] underline"
+                  id="job-title"
+                  data-testid="job-title"
+                >
+                  {applicantInfo?.job?.title}
+                </p>
+                <div className="mt-[4px] flex gap-[8px]">
+                  <span
+                    className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
+                    id="job-type"
+                    data-testid="job-type"
+                  >
+                    <BriefcaseBusiness className="size-[16px]" />
+                    Fulltime
+                  </span>
+                  <span
+                    className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
+                    id="job-location"
+                    data-testid="job-location"
+                  >
+                    <MapPin className="size-[16px]" />
+                    {country && country.length > 0 ? (country as CountryItem[]).find(item => item.id === applicantInfo?.job?.country_id)?.name || 'Unknown' : 'Unknown'}
+                  </span>
+                  <span
+                    className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
+                    id="submission-date"
+                    data-testid="submission-date"
+                  >
+                    <Clock className="size-[16px]" />
+                    {moment(applicantInfo?.job?.deadline).format('MMMM DD YYYY')}
+                  </span>
+                </div>
+              </>
+            )}
           </div>
-          <span
-            className="pl-[3px] pr-[8px] py-[1px] bg-[#8f8f8f] text-[#fff] text-[12px]/[22px] rounded-[30px] flex items-center"
-            id="application-status"
-            data-testid="application-status"
-          >
-            <Dot className="size-[20px]" />
-            {applicantInfo?.status.charAt(0).toUpperCase() + applicantInfo?.status.slice(1)}
-          </span>
+          {loading ? (
+            <Skeleton className="h-[22px] w-[100px] bg-gray-200 rounded-[30px]" />
+          ) : (
+            <span
+              className="pl-[3px] pr-[8px] py-[1px] bg-[#8f8f8f] text-[#fff] text-[12px]/[22px] rounded-[30px] flex items-center"
+              id="application-status"
+              data-testid="application-status"
+            >
+              <Dot className="size-[20px]" />
+              {applicantInfo?.status.charAt(0).toUpperCase() + applicantInfo?.status.slice(1)}
+            </span>
+          )}
         </div>
 
-        {loading ? <>
-          <div className="flex items-center justify-center mt-[30px] pb-[20px]">
-            <LoadingSpinner />
-          </div>
-        </> : <div
-          className="pl-[40px] pr-[77px] pt-[30px] flex flex-col gap-[36px] pb-[20px]"
-          id="application-sections"
-          data-testid="application-sections"
-        >
-          {/* Contact Info Section */}
-          <div>
-            <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Contact Info</p>
-            <p className="text-[14px]/[22px] text-[#a5a5a5]">Full Name</p>
-            <p className="text-[14px]/[22px] text-[#353535]">
-              {applicantInfo?.applicant?.first_name} {applicantInfo?.applicant?.last_name}
-            </p>
-            <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Email Address</p>
-            <p className="text-[14px]/[22px] text-[#353535]">{applicantInfo?.applicant?.email}</p>
-            <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Address</p>
-            <p className="text-[14px]/[22px] text-[#353535]">
-              {applicantInfo?.address}, {applicantInfo?.city}, {applicantInfo?.state} {applicantInfo?.zip_code}
-            </p>
-            <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Phone Number</p>
-            <p className="text-[14px]/[22px] text-[#353535]">
-              {applicantInfo?.contact_code} {applicantInfo?.contact_number}
-            </p>
-          </div>
-
-          <div>
-            <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Resume/Cover Letter</p>
-            {applicantInfo?.cv_media && (
-              <p className="text-[14px]/[22px] text-[#353535]">CV: {applicantInfo.cv_media.original_name}</p>
-            )}
-            {applicantInfo?.cover_media && (
-              <p className="text-[14px]/[22px] text-[#353535]">Cover Letter: {applicantInfo.cover_media.original_name}</p>
-            )}
-          </div>
-
-          <div>
-            <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Experience</p>
-            {applicantInfo?.work_experience?.map((work: any, index: number) => (
-              <div key={index} className="mt-[8px]">
-                <p className="text-[14px]/[22px] text-[#a5a5a5]">Experience {index + 1}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{work.job_title} at {work.company}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{work.location}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">
-                  {new Date(work.from_date).toLocaleDateString()} - {work.is_currently_working ? 'Present' : new Date(work.to_date).toLocaleDateString()}
-                </p>
-                <p className="text-[14px]/[22px] text-[#4b4b4b]">{work.role_description}</p>
-              </div>
-            ))}
-            {applicantInfo?.education?.map((edu: any, index: number) => (
-              <div key={index} className="mt-[8px]">
-                <p className="text-[14px]/[22px] text-[#a5a5a5]">Education {index + 1}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{edu.school}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{edu.degree.name}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">
-                  {edu.field_of_study}
-                </p>
-              </div>
-            ))}
-            {applicantInfo?.certificate?.map((cert: any, index: number) => (
-              <div key={index} className="mt-[8px]">
-                <p className="text-[14px]/[22px] text-[#a5a5a5]">Certification {index + 1}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{cert.title}</p>
-                <p className="text-[14px]/[22px] text-[#353535] font-medium">{cert.number}</p>
-
-              </div>
-            ))}
-            <div className="mt-[8px]">
-              <p className="text-[14px]/[22px] text-[#a5a5a5]">Skills</p>
-              <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.skills.map((skill: any, index: number) => <span key={index}>{skill.name}</span>)}</p>
+        {loading ? (
+          <div
+            className="pl-[40px] pr-[77px] pt-[30px] flex flex-col gap-[36px] pb-[20px]"
+            id="application-sections"
+            data-testid="application-sections"
+          >
+            {/* Contact Info Section Skeleton */}
+            <div>
+              <Skeleton className="h-[24px] w-[120px] bg-gray-200 mb-2" />
+              <Skeleton className="h-[22px] w-[80px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[200px] bg-gray-200 mb-3" />
+              <Skeleton className="h-[22px] w-[100px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[180px] bg-gray-200 mb-3" />
+              <Skeleton className="h-[22px] w-[70px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[250px] bg-gray-200 mb-3" />
+              <Skeleton className="h-[22px] w-[100px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[150px] bg-gray-200" />
             </div>
-            <div className="mt-[8px]">
-              <p className="text-[14px]/[22px] text-[#a5a5a5]">LinkedIn</p>
-              <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.linkedin_link}</p>
+
+            {/* Resume/Cover Letter Section Skeleton */}
+            <div>
+              <Skeleton className="h-[24px] w-[150px] bg-gray-200 mb-2" />
+              <Skeleton className="h-[22px] w-[120px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[180px] bg-gray-200" />
             </div>
-            <div className="mt-[8px]">
-              <p className="text-[14px]/[22px] text-[#a5a5a5]">Website/Portfolio Link</p>
-              <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.portfolio_link}</p>
+
+            {/* Experience Section Skeleton */}
+            <div>
+              <Skeleton className="h-[24px] w-[100px] bg-gray-200 mb-2" />
+              <Skeleton className="h-[22px] w-[80px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[200px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[150px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[120px] bg-gray-200 mb-3" />
+              <Skeleton className="h-[22px] w-[250px] bg-gray-200" />
+            </div>
+
+            {/* Applicant Questions Section Skeleton */}
+            <div>
+              <Skeleton className="h-[24px] w-[160px] bg-gray-200 mb-2" />
+              <Skeleton className="h-[22px] w-[100px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[300px] bg-gray-200 mb-3" />
+              <Skeleton className="h-[22px] w-[120px] bg-gray-200 mb-1" />
+              <Skeleton className="h-[22px] w-[280px] bg-gray-200" />
             </div>
           </div>
+        ) : (
+          <div
+            className="pl-[40px] pr-[77px] pt-[30px] flex flex-col gap-[36px] pb-[20px]"
+            id="application-sections"
+            data-testid="application-sections"
+          >
+            {/* Contact Info Section */}
+            <div>
+              <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Contact Info</p>
+              <p className="text-[14px]/[22px] text-[#a5a5a5]">Full Name</p>
+              <p className="text-[14px]/[22px] text-[#353535]">
+                {applicantInfo?.applicant?.first_name} {applicantInfo?.applicant?.last_name}
+              </p>
+              <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Email Address</p>
+              <p className="text-[14px]/[22px] text-[#353535]">{applicantInfo?.applicant?.email}</p>
+              <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Address</p>
+              <p className="text-[14px]/[22px] text-[#353535]">
+                {applicantInfo?.address}, {applicantInfo?.city}, {applicantInfo?.state} {applicantInfo?.zip_code}
+              </p>
+              <p className="text-[14px]/[22px] text-[#a5a5a5] mt-[14px]">Phone Number</p>
+              <p className="text-[14px]/[22px] text-[#353535]">
+                {applicantInfo?.contact_code} {applicantInfo?.contact_number}
+              </p>
+            </div>
 
-          <div>
-            <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Applicant Questions</p>
-            {questions.map((question: any) => {
-              let answer = '';
-              if (question.applicant_answer) {
-                try {
-                  const finalAnswer = question.applicant_answer.split('\"')[1]
-                  answer = finalAnswer;
-                } catch (error) {
-                  answer = question.applicant_answer;
-                }
-              }
+            <div>
+              <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Resume/Cover Letter</p>
+              {applicantInfo?.cv_media && (
+                <p className="text-[14px]/[22px] \
+                -[#353535]">CV: {applicantInfo.cv_media.original_name}</p>
+              )}
+              {applicantInfo?.cover_media && (
+                <p className="text-[14px]/[22px] text-[#353535]">Cover Letter: {applicantInfo.cover_media.original_name}</p>
+              )}
+            </div>
 
-              return (
-                <div key={question.id} className="mt-[8px]">
-                  <p className="text-[14px]/[22px] text-[#a5a5a5]">{question.question_name}</p>
-                  <p className="text-[14px]/[22px] text-[#353535] wrap-anywhere">{answer}</p>
+            <div>
+              <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Experience</p>
+              {applicantInfo?.work_experience?.map((work: any, index: number) => (
+                <div key={index} className="mt-[8px]">
+                  <p className="text-[14px]/[22px] text-[#a5a5a5]">Experience {index + 1}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{work.job_title} at {work.company}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{work.location}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">
+                    {new Date(work.from_date).toLocaleDateString()} - {work.is_currently_working ? 'Present' : new Date(work.to_date).toLocaleDateString()}
+                  </p>
+                  <p className="text-[14px]/[22px] text-[#4b4b4b]">{work.role_description}</p>
                 </div>
-              );
-            })}
+              ))}
+              {applicantInfo?.education?.map((edu: any, index: number) => (
+                <div key={index} className="mt-[8px]">
+                  <p className="text-[14px]/[22px] text-[#a5a5a5]">Education {index + 1}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{edu.school}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{edu.degree.name}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">
+                    {edu.field_of_study}
+                  </p>
+                </div>
+              ))}
+              {applicantInfo?.certificate?.map((cert: any, index: number) => (
+                <div key={index} className="mt-[8px]">
+                  <p className="text-[14px]/[22px] text-[#a5a5a5]">Certification {index + 1}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{cert.title}</p>
+                  <p className="text-[14px]/[22px] text-[#353535] font-medium">{cert.number}</p>
+
+                </div>
+              ))}
+              <div className="mt-[8px]">
+                <p className="text-[14px]/[22px] text-[#a5a5a5]">Skills</p>
+                <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.skills.map((skill: any, index: number) => <span key={index}>{skill.name}</span>)}</p>
+              </div>
+              <div className="mt-[8px]">
+                <p className="text-[14px]/[22px] text-[#a5a5a5]">LinkedIn</p>
+                <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.linkedin_link}</p>
+              </div>
+              <div className="mt-[8px]">
+                <p className="text-[14px]/[22px] text-[#a5a5a5]">Website/Portfolio Link</p>
+                <p className="text-[14px]/[22px] text-[#353535] font-medium">{applicantInfo?.portfolio_link}</p>
+              </div>
+            </div>
+
+            <div>
+              <p className="text-[16px]/[24px] font-medium text-[#1c1c1c]">Applicant Questions</p>
+              {questions.map((question: any) => {
+                let answer = '';
+                if (question.applicant_answer) {
+                  try {
+                    const finalAnswer = question.applicant_answer.split('\"')[1]
+                    answer = finalAnswer;
+                  } catch (error) {
+                    answer = question.applicant_answer;
+                  }
+                }
+
+                return (
+                  <div key={question.id} className="mt-[8px]">
+                    <p className="text-[14px]/[22px] text-[#a5a5a5]">{question.question_name}</p>
+                    <p className="text-[14px]/[22px] text-[#353535] wrap-anywhere">{answer}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>}
+        )}
       </div>
     </div>
   );
--- app/applicant/(dashboard)/my-applications/page.tsx
+++ app/applicant/(dashboard)/my-applications/page.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 import { getJobInfo } from '@/api/applicant';
 import { useSession } from '@/context/SessionContext';
+import { LoadingSpinner } from '@/components/common/loading-spinner';
 
 interface JobApplication {
   id: number;
@@ -36,14 +37,22 @@ export default function MyApplications() {
    * Applies dynamic styles and conditions based on application status.
    */
   const [data, setData] = React.useState<JobApplication[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
   const router = useRouter();
   const { session } = useSession();
   const [applicant_id, setApplicantId] = React.useState<string | null>(null);
 
   const getJobData = async () => {
     if (!applicant_id) return;
-    const response = await getJobInfo(undefined, applicant_id);
-    setData(response.data);
+    setLoading(true);
+    try {
+      const response = await getJobInfo(undefined, applicant_id);
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching job data:', error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -82,86 +91,93 @@ export default function MyApplications() {
       >
         Welcome {session.full_name},
       </p>
-      <div
-        className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[22px] mt-[22px]"
-        id="applications-grid"
-        data-test-id="applications-grid"
-      >
-        {data.map((item, index) => (
-          <div
-            className="w-full border border-[#e9e9e9] bg-white pt-[20px] pb-[24px] px-[24px] rounded-[8px]"
-            key={index}
-            id={`application-card-${item.id}`}
-            data-test-id={`application-card-${item.id}`}
-          >
-            <div className="w-full flex justify-between">
-              <span
-                className={`pl-[3px] pr-[8px] py-[1px] ${item.status !== 'not submitted' ? 'text-white' : 'text-[#1c1c1c]'
-                  } text-[12px]/[22px] ${colors[item.status] || colors['rejected']} rounded-[30px] flex items-center`}
-                id={`status-pill-${item.id}`}
-                data-test-id={`status-pill-${item.id}`}
-              >
-                <Dot className="size-[20px]" /> {item.status.charAt(0).toUpperCase() + item.status.slice(1)}
-              </span>
-              <ActionsCell />
-            </div>
-            <p
-              className="mt-[10px] text-[18px]/[30px] font-semibold"
-              id={`job-title-${item.id}`}
-              data-test-id={`job-title-${item.id}`}
+
+      {loading ? (
+        <div className="flex justify-center items-center mt-[50px]">
+          <LoadingSpinner />
+        </div>
+      ) : (
+        <div
+          className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[22px] mt-[22px]"
+          id="applications-grid"
+          data-test-id="applications-grid"
+        >
+          {data.map((item, index) => (
+            <div
+              className="w-full border border-[#e9e9e9] bg-white pt-[20px] pb-[24px] px-[24px] rounded-[8px]"
+              key={index}
+              id={`application-card-${item.id}`}
+              data-test-id={`application-card-${item.id}`}
             >
-              {item?.job?.title}
-            </p>
-            <div className="mt-[4px] flex gap-[8px]">
-              <span
-                className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
-                id={`job-type-${item.id}`}
-                data-test-id={`job-type-${item.id}`}
+              <div className="w-full flex justify-between">
+                <span
+                  className={`pl-[3px] pr-[8px] py-[1px] ${item.status !== 'not submitted' ? 'text-white' : 'text-[#1c1c1c]'
+                    } text-[12px]/[22px] ${colors[item.status] || colors['rejected']} rounded-[30px] flex items-center`}
+                  id={`status-pill-${item.id}`}
+                  data-test-id={`status-pill-${item.id}`}
+                >
+                  <Dot className="size-[20px]" /> {item.status.charAt(0).toUpperCase() + item.status.slice(1)}
+                </span>
+                <ActionsCell />
+              </div>
+              <p
+                className="mt-[10px] text-[18px]/[30px] font-semibold"
+                id={`job-title-${item.id}`}
+                data-test-id={`job-title-${item.id}`}
               >
-                <BriefcaseBusiness className="size-[16px]" />
-                {item?.job?.location_type?.name}
-              </span>
-              <span
-                className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
-                id={`job-location-${item.id}`}
-                data-test-id={`job-location-${item.id}`}
-              >
-                <MapPin className="size-[16px]" />
-                {item?.job?.country?.name}
-              </span>
+                {item?.job?.title}
+              </p>
+              <div className="mt-[4px] flex gap-[8px]">
+                <span
+                  className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
+                  id={`job-type-${item.id}`}
+                  data-test-id={`job-type-${item.id}`}
+                >
+                  <BriefcaseBusiness className="size-[16px]" />
+                  {item?.job?.location_type?.name}
+                </span>
+                <span
+                  className="text-[12px]/[18px] flex items-center gap-[2px] text-[#787878]"
+                  id={`job-location-${item.id}`}
+                  data-test-id={`job-location-${item.id}`}
+                >
+                  <MapPin className="size-[16px]" />
+                  {item?.job?.country?.name}
+                </span>
+              </div>
+              <div className="mt-[8px] flex gap-[4px]">
+                <Clock className="size-[16px] text-[#8f8f8f]" />
+                <span
+                  className="text-[12px]/[18px] text-[#8f8f8f]"
+                  id={`applied-date-${item.id}`}
+                  data-test-id={`applied-date-${item.id}`}
+                >
+                  Applied : {moment(item.created_at).format('MMMM DD YYYY')}
+                </span>
+              </div>
+              {item.status === 'not submitted' ? (
+                <Button
+                  className="mt-[18px] w-full h-[32px]"
+                  id={`continue-btn-${item.id}`}
+                  data-test-id={`continue-btn-${item.id}`}
+                >
+                  Continue Application
+                </Button>
+              ) : (
+                <Button
+                  variant="outline"
+                  className="mt-[18px] w-full h-[32px]"
+                  id={`view-btn-${item.id}`}
+                  data-test-id={`view-btn-${item.id}`}
+                  onClick={() => router.push(`/applicant/my-applications/${item.job_id}`)}
+                >
+                  View Application
+                </Button>
+              )}
             </div>
-            <div className="mt-[8px] flex gap-[4px]">
-              <Clock className="size-[16px] text-[#8f8f8f]" />
-              <span
-                className="text-[12px]/[18px] text-[#8f8f8f]"
-                id={`applied-date-${item.id}`}
-                data-test-id={`applied-date-${item.id}`}
-              >
-                Applied : {moment(item.created_at).format('MMMM DD YYYY')}
-              </span>
-            </div>
-            {item.status === 'not submitted' ? (
-              <Button
-                className="mt-[18px] w-full h-[32px]"
-                id={`continue-btn-${item.id}`}
-                data-test-id={`continue-btn-${item.id}`}
-              >
-                Continue Application
-              </Button>
-            ) : (
-              <Button
-                variant="outline"
-                className="mt-[18px] w-full h-[32px]"
-                id={`view-btn-${item.id}`}
-                data-test-id={`view-btn-${item.id}`}
-                onClick={() => router.push(`/applicant/my-applications/${item.job_id}`)}
-              >
-                View Application
-              </Button>
-            )}
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
